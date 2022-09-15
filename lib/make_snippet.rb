5aa18e4ab58f46d0ac67c35de5d63336e2a0e2b1
Ali <abhatti89@gmail.com>
2022-09-15 15:14:30 +0100

    Make Snippet 3 RSpec tests

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,3 +1,15 @@
 def make_snippet(diary)
-  return ""
+  return diary
+  text = diary.split(" ")
+  if text.count <= 5
+    puts diary
+  else
+    puts text[0,5].join(" ") + "..."
+  end
 end
+
+string = "Today is the 15th of September"
+
+text = string.split(" ")
+
+puts text[0,5].join(" ") + "..."
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,10 +1,15 @@
 require "make_snippet"
 
 RSpec.describe "make_snippet_method" do
-  it "if passed an empty string, it returns it" do
+  it "returns an empty string if passed an empty string" do
     expect(make_snippet("")).to eq ""
   end
+
+  it "returns the diary entry if it's already less than 6 words" do
+    expect(make_snippet("Hello, today is Thursday.")).to eq "Hello, today is Thursday."
+  end
+
   it "reduces a string to only 5 words followed by \"...\"" do
-    expect(make_snippet("Today is the 15th of September.")).to eq "Today is the 15th of..."
+    expect(make_snippet("Today is the 15th of September")).to eq "Today is the 15th of..."
   end
 end
