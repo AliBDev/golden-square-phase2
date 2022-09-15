cc69ace020fca3ac3818848946e671f2f94c4e09
Ali <abhatti89@gmail.com>
2022-09-15 12:26:53 +0100

    Make Snippet 2 RSpec tests

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,16 +1,3 @@
-#   def make_snippet(str)
-#       entry = []
-#       str << entry
-#       splitarray = entry.split (", ")
-#       entry_count = splitarray.count
-
-#     if entry_count == =<4
-#   end
-
-# word = "I had a great day today"
-# splitarray = word.split (" ")
-# puts splitarray.count
-
 def make_snippet(diary)
   return ""
 end
